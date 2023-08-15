33b5b78b37e5a2fc7b6f38caf4b7522654b2a33a
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-08-15 10:11:04 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
 #include "main.h"
-/*This is the main function*/
-int main (void)
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
 {
-_putchar("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
+
