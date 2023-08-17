aafc1bd7296af300b19ac55bb107492503663ad7
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-08-17 09:23:47 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,35 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code.
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int main(void)
-{
-char c;
-c = 'A';
-_putchar('A');
-_putchar(':');
-_putchar(' ');
-_putchar(_isupper(c));
-_putchar('\n');
-c = 'a';
-_putchar('A');
-_putchar(':');
-_putchar(' ');
-_putchar(_isupper(c));
-_putchar('\n');
-return (0);
-}
+
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-else
-{
-return (0);
-}
-}
+
