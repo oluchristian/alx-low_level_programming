a8efe44f3aad865a83dbc6414dfa86fe8c2d0977
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-08-28 17:59:40 -0700

    mandatory

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer to char
+ * @s: the pointer to pointer
+ * @to: the pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
