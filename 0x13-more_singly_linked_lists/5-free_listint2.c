5a696a216b80082bda4cb6340faf39492c13526e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-09-26 11:41:57 -0700

    frees memory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list.
+ * @head: head node
+ *
+ * Description: func to free a listint_t list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	*head = NULL;
+
+	while (current != NULL)
+	{
+		listint_t *temp = current;
+
+		current = temp->next;
+
+		free(temp);
+	}
+}
+
