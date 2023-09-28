746d361c33cd9cf3339939a00e56a73a491bf549
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-09-27 20:42:08 -0700

    deletes a node at n index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - func to delete node at n index
+ * @head: head pointer
+ * @index: index of the node to delete
+ *
+ * @Return - returns -1 or 1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *previous;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+	previous = current;
+	current = current->next;
+		
+	while (current != NULL && count < index)
+	{
+		previous = current;
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+		return (-1);
+	previous->next = current->next;
+	free(current);
+	return (1);
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
+ * Description: 'the program's description'
  * @head: points to the first element in the list
  * @idx: index to insert
  * @n: int of the node element
- *
  * @Return - address of the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
