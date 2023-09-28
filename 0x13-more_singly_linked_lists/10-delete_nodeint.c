18d7e416837e0ab92f6ab2dad0383c7852a7c21b
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-09-27 20:43:12 -0700

    deletes a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,8 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	previous = current;
-	current = current->next;
-		
+	current = current->next;		
 	while (current != NULL && count < index)
 	{
 		previous = current;
