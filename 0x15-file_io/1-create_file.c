b2bd463d6ea36c353b3753641a149da7ceddd906
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-10-05 16:06:19 -0700

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,24 +13,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	file = open(filename, O_CREAT | O_TRUNC, 0600);
 
 	if (file == -1)
 	{
 		close(file);
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		while (text_content[count] != '\0')
+		{
+			count++;
+		}
 	}
-	while (*text_content != '\0')
-	{
-		text_content++;
-		count++;
-	}
-	text_content -= count;
 	w = write(file, text_content, count);
 
 	if (w == -1)
