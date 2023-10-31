71f56047ba5dbd53cfc212d9f33d489c3e49a40c
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-10-30 21:06:41 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,79 +2,71 @@
 #include <stdio.h>
 
 /**
- * check_file_opening - Checks if files can be opened and handles errors.
- * @file_descriptor_from: File descriptor for the source file.
- * @file_descriptor_to: File descriptor for the destination file.
- * @file_from_path: Path to the source file.
- * @file_to_path: Path to the destination file.
- *
- * This function checks if the source and destination files can be opened
- * and handles errors by printing appropriate messages and exiting the program.
- *
- * Return: No return.
+ * error_file - checks if files can be opened.
+ * @file_from: from file.
+ * @file_to: to file.
+ * @argv: arguments array.
+ * Return: no return.
  */
-void check_file_opening(int file_descriptor_from, int file_descriptor_to, char *file_from_path, char *file_to_path)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_descriptor_from == -1)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from_path);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_descriptor_to == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to_path);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - Copies the content of one file to another file.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * Return: Always returns 0.
+ * main - main func
+ * @argc: num of arguments.
+ * @argv: arguments array.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int source_file_descriptor, destination_file_descriptor, close_error;
-	ssize_t num_chars_read, num_chars_written;
-	char buffer[1024];
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	source_file_descriptor = open(argv[1], O_RDONLY);
-	destination_file_descriptor = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	check_file_opening(source_file_descriptor, destination_file_descriptor, argv[1], argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	num_chars_read = 1024;
-	while (num_chars_read == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		num_chars_read = read(source_file_descriptor, buffer, 1024);
-		if (num_chars_read == -1)
-			check_file_opening(-1, 0, argv[1], argv[2]);
-		num_chars_written = write(destination_file_descriptor, buffer, num_chars_read);
-		if (num_chars_written == -1)
-			check_file_opening(0, -1, argv[1], argv[2);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
 
-	close_error = close(source_file_descriptor);
-	if (close_error == -1)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_file_descriptor);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	close_error = close(destination_file_descriptor);
-	if (close_error == -1)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destination_file_descriptor);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	return (0);
 }
-
