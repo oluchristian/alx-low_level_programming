afff6197f9491809ef3aeefbd67e0f92a80ca7d0
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-06 13:14:06 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,10 @@
 #define __MAIN_H__
 
 #include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
