87d1d2e533b24bba49addd50bd9b49bac1bb67e9
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-23 06:17:22 -0800

    print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	unsigned long int i;
+	int printed = 0;
 
 	if (ht == NULL)
 	{
@@ -20,16 +21,13 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			if (node->next != NULL)
+			if (printed == 1)
 			{
 				printf(", ");
 			}
+			printf("'%s': '%s'", node->key, node->value);
+			printed = 1;
 			node = node->next;
-		}
-		if (i < ht->size - 1 && ht->array[i + 1] != NULL)
-		{
-			printf(", ");
 		}
 	}
 	printf("}\n");
