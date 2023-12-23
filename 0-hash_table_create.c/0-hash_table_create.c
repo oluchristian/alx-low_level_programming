cde0d72edbf57e158dc2680d6e034083678145ba
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-22 19:00:22 -0800

    created hash table

--- 0-hash_table_create.c/0-hash_table_create.c
+++ 0-hash_table_create.c/0-hash_table_create.c
@@ -8,18 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned int i = 0;
 	hash_table_t *ht = (hash_table_t *)(malloc(sizeof(hash_table_t)));
 
-	if (ht == Null)
+	if (ht == NULL)
 	{
-		return (Null);
+		return (NULL);
 	}
 	ht->size = size;
 	ht->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
-	if (ht->array == Null)
+	if (ht->array == NULL)
 	{
 		free(ht);
-		return (Null);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
