ee5e3694bec16044deb5a4c0cf5124a0363da5ae
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-22 19:11:42 -0800

    hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: the size, in number of nodes, to make the new hash table.
+ *
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *ht = (hash_table_t *)(malloc(sizeof(hash_table_t)));
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+	return (ht);
+}
+
