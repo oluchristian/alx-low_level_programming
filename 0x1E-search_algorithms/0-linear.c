2e4ef93cce1dc97ad7cd7a0fb3fa667cd6e4c3f2
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-12 03:44:27 +0100

    reformatted the code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,10 +16,8 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
+		if (value == array[i])
 			return (i);
-		}
 	}
 	return (-1);
 }
